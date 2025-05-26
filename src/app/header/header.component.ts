5c8dddff7633a4501cf827f04772d9d6497ee7cd
Shwetha <Shwetha.Venkatesh@itorizon.com>
2025-05-26 22:35:14 +0530

    second

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:"app-header",
-    template:"<h1>Hi</h1>",
+    //template:"<h1>Hi</h1>",
+    templateUrl:"./header.component.html",
+    styleUrls:["./header.component.css"]
 })
 export class HeaderComponent {
 }
